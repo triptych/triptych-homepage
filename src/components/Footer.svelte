cdfdefa0ff4b9fec706ca6fa45afe5afbfe123a1
Andrew Wooldridge <triptych@gmail.com>
2021-11-21 22:35:48 -0800

    making more links and stuff

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,12 +1,17 @@
 <footer>
+    <hr/>
     <ul>
         <li><a href="mailto:triptych@gmail.com">email me</a></li>
     </ul>
+    <p>Copyright &copy; Andrew Wooldridge  2021</p>
 </footer>
 <style>
     ul {
         display: flex;
         font-size:small;
         list-style-type: none;
+    }
+    p {
+        text-align: center;
     }
 </style>
