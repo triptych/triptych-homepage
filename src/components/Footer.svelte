a9ed9781fb45a89818c400f716fbd7ec6ec50065
Andrew Wooldridge <triptych@gmail.com>
2021-11-21 18:59:37 -0800

    code changes for the site

---
+++ src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<footer>
+    <ul>
+        <li><a href="mailto:triptych@gmail.com">email me</a></li>
+    </ul>
+</footer>
+<style>
+    ul {
+        display: flex;
+        font-size:small;
+        list-style-type: none;
+    }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,22 @@
+<header>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/blog">Blog</a></li>
+            <li><a href="/links">Links</a></li>
+        </ul>      
+    </nav>
+</header>
+<style>
+    ul {
+        list-style-type: none;
+        display: flex; 
+    }
+    ul li a{
+        border: 1px solid black;
+        margin-right: 8px;
+        padding: 4px;
+        border-radius: 2px;
+    }
+</style>
